d5c91113cd7bcc560b68c51f391827bee5b71218
Nitro <nitro8985@gmail.com>
2022-10-10 15:54:11 +0600

    cleanup

--- main.go
+++ main.go
@@ -2,41 +2,6 @@ package main
 
 import "fmt"
 
-// import (
-// 	"os"
-
-// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-// )
-
-// func main() {
-// 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// bot.Debug = true
-
-// 	updateConfig := tgbotapi.NewUpdate(0)
-
-// 	updateConfig.Timeout = 30
-
-// 	updates := bot.GetUpdatesChan(updateConfig)
-
-// 	for update := range updates {
-// 		if update.Message == nil {
-// 			continue
-// 		}
-
-// 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-// 		msg.ReplyToMessageID = update.Message.MessageID
-
-// 		if _, err := bot.Send(msg); err != nil {
-// 			panic(err)
-// 		}
-// 	}
-// }
-
 func main() {
 	game := new(ttt_game)
 	for Play(game) {
